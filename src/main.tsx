d18f121405f8c6404f5fdee1dd507c78c17c7fed
Adaamss <aademhamdi@gmail.com>
2026-08-06 23:54:35 +0100

    chore: update entry point for stepper component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Stepper } from "./Stepper";
+import { Stepper } from "./components/stepper/Stepper";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
