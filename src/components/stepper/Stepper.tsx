f94137fc3dd7b3ffe0111985328bcfbf6992a552
Adaamss <aademhamdi@gmail.com>
2026-08-10 23:50:37 +0100

    feat: wire OrderStep with quantity and addon toggle handlers

--- src/components/stepper/Stepper.tsx
+++ src/components/stepper/Stepper.tsx
@@ -1,11 +1,18 @@
 import React, { useReducer } from "react";
 import { initialState, stepperReducer } from "./reducer";
-import { SET_ATTENDEE_FIELD, SET_SELECTION } from "./actions";
+import {
+  SET_ATTENDEE_FIELD,
+  SET_ORDER_FIELD,
+  SET_SELECTION,
+  TOGGLE_ADDON,
+} from "./actions";
 import { PersonalInfoStep } from "../stepper-steps/personalInfoStep/PersonalInfoStep";
 import { ContactStep } from "../stepper-steps/contactStep/ContactStep";
 import { EventTierStep } from "../stepper-steps/eventTierStep/EventTierStep";
+import { OrderStep } from "../stepper-steps/orderStep/OrderStep";
 export const Stepper = () => {
   const [state, dispatch] = useReducer(stepperReducer, initialState);
+  console.log(state);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({
@@ -13,6 +20,27 @@ export const Stepper = () => {
       payload: {
         name: e.target.name,
         value: e.target.value,
+      },
+    });
+  };
+
+  const handleTicketQuantityChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    dispatch({
+      type: SET_ORDER_FIELD,
+      payload: {
+        name: e.target.name,
+        value: e.target.value,
+      },
+    });
+  };
+
+  const handleAddonCheckBox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: TOGGLE_ADDON,
+      payload: {
+        addonId: e.target.value,
       },
     });
   };
@@ -52,6 +80,13 @@ export const Stepper = () => {
         selection={state.selection}
         handleEventSelection={handleEventSelect}
         handleTierSelection={handleSelectTier}
+      />
+
+      <OrderStep
+        order={state.order}
+        handleTicketQuantity={handleTicketQuantityChange}
+        addons={state.addons}
+        handleCheckBoxToggle={handleAddonCheckBox}
       />
     </>
   );
