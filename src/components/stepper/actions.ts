286dc3c0f8460a82164d766f55b04139b83c13eb
Adaamss <aademhamdi@gmail.com>
2026-08-10 23:50:10 +0100

    feat: add TOGGLE_ADDON action

--- src/components/stepper/actions.ts
+++ src/components/stepper/actions.ts
@@ -3,3 +3,4 @@ export const SET_ORDER_FIELD = "set_order_field";
 export const SET_SELECTION = "set_selection";
 export const NEXT_STEP = "next_step";
 export const PREV_STEP = "prev_step";
+export const TOGGLE_ADDON = "toggle_addon";
