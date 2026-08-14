7aa585e744c0b670abeb8056a1f62badfabf19e5
Adaamss <aademhamdi@gmail.com>
2026-08-14 13:37:11 +0100

    feat: add RootLayout with header and outlet

---
+++ src/components/rootLayout/RootLayout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../header/Header";
+import styled from "styled-components";
+
+const RootWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+`;
+
+export const RootLayout = () => {
+  return (
+    <RootWrapper>
+      <Header />
+      <MainContent>
+        <Outlet />
+      </MainContent>
+    </RootWrapper>
+  );
+};
