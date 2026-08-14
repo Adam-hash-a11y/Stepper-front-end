cf4d9ddf848dc4f69ba1af231fa922c68d47bebc
Adaamss <aademhamdi@gmail.com>
2026-08-14 15:30:44 +0100

    style: add form input error styling

--- src/components/shared/inputError/InputError.tsx
+++ src/components/shared/inputError/InputError.tsx
@@ -1,7 +1,17 @@
+import styled from "styled-components";
+
 interface Props {
   error: string;
 }
 
+const ErrorText = styled.p`
+  font-size: 11px;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+  color: #dc2626;
+  margin-top: 6px;
+`;
+
 export const FormInputError: React.FunctionComponent<Props> = ({ error }) => {
-  return <p>{error}</p>;
+  return <ErrorText>{error}</ErrorText>;
 };
