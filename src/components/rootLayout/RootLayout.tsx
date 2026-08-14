7c823f873d6f1382274f71eed03499692196f33e
Adaamss <aademhamdi@gmail.com>
2026-08-14 13:44:00 +0100

    chore: add footer to rootlayout

--- src/components/rootLayout/RootLayout.tsx
+++ src/components/rootLayout/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../header/Header";
 import styled from "styled-components";
+import { Footer } from "../footer/Footer";
 
 const RootWrapper = styled.div`
   display: flex;
@@ -19,6 +20,7 @@ export const RootLayout = () => {
       <MainContent>
         <Outlet />
       </MainContent>
+      <Footer />
     </RootWrapper>
   );
 };
