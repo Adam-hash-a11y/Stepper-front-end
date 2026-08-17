f1316b55f49c86b328e2bf0f3575c832f7a54596
Adaamss <aademhamdi@gmail.com>
2026-08-17 23:37:01 +0100

    feat: add home page and booking routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { RootLayout } from "../components/rootLayout/RootLayout";
 import { Stepper } from "../components/stepper/Stepper";
 import { createGlobalStyle } from "styled-components";
 import { ToastContainer } from "react-toastify";
+import { HomePage } from "../components/homePage/HomePage";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -25,7 +26,8 @@ export const App = () => {
       <ToastContainer theme="dark" position="top-right" />
       <Routes>
         <Route element={<RootLayout />}>
-          <Route index element={<Stepper />} />
+          <Route index element={<HomePage />} />
+          <Route path="booking" element={<Stepper />} />
         </Route>
       </Routes>
     </>
