242a22dde1951ee859d5870d7efb5e8e6cec24f6
Adaamss <aademhamdi@gmail.com>
2026-09-05 15:37:12 +0100

    chore: configure i18next and add en, fr, de locale files

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,16 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en/translation.json";
 import frTranslation from "./locales/fr/translation.json";
+import deTranslation from "./locales/de/translation.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: enTranslation },
     fr: { translation: frTranslation },
+    de: { translation: deTranslation },
   },
-  lng: "en", // Default language
+  lng: "en",
   fallbackLng: "en",
   interpolation: {
-    escapeValue: false, // React already escapes values
+    escapeValue: false,
   },
 });
 
